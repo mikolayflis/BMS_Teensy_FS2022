b012eb115119b9126b10c04f2b54b156d58df13f
mikolayflis <57568131+mikolayflis@users.noreply.github.com>
2023-03-23 15:20:56 +0000

    Reads voltage, converts to temp, sends to serial

--- AnalogInputTest.ino
+++ AnalogInputTest.ino
@@ -22,10 +22,10 @@ int ledPin = 13;      // select the pin for the LED
   // Pin 13: Teensy 3.0 has the LED on pin 13 
   
 // variables to store the value coming from the sensor
-int ADCs[10];         //storing values from ADCS
-float Voltages[10];   //storing values from ADCS converted to voltage
+int ADCs[32];         //storing values from ADCS
+float Voltages[32];   //storing values from ADCS converted to voltage
                       //arduino ADC returns values in integer from 0 to 1023
-float Temperatures[10];//storing values from ADCS converted to temperature in degrees Celsius
+float Temperatures[32];//storing values from ADCS converted to temperature in degrees Celsius
 
 int maxValueADCindex = 0;
 float maxVoltage = 0.0;
@@ -39,7 +39,8 @@ void setup() {
   pinMode(MUX_S3, OUTPUT);
 
   //set S1, S2, and S3 to 1
-  //digitalWrite(MUX_S1, 1);
+  digitalWrite(MUX_S0, 0);
+  digitalWrite(MUX_S1, 0);
   digitalWrite(MUX_S2, 0);
   digitalWrite(MUX_S3, 0);
 
@@ -47,7 +48,7 @@ void setup() {
   Serial.begin(9600);
 
   //reset values in array that stores voltage readings
-  for(int i = 0; i<10; i++)
+  for(int i = 0; i<32; i++)
   {
       ADCs[i] = 0;
       Voltages[i] = 0.0;
@@ -63,12 +64,14 @@ void loop() {
   
   // read the value from the sensor:
   //(simultaneously take note of the largest one?)
-  for(int i = 0b0; i<0b10000; i++)
+  for(int i = 0b0; i<0b10; i++)
    {  
       digitalWrite(MUX_S0, i&0b0001); //LSB of i controls S0
-      digitalWrite(MUX_S1, i&0b0010); //2nd bit of i controls S1
-      digitalWrite(MUX_S2, i&0b0100); //3nd bit of i controls S2
-      digitalWrite(MUX_S2, i&0b1000); //3nd bit of i controls S2
+      //digitalWrite(MUX_S1, i&0b0010); //2nd bit of i controls S1
+      //digitalWrite(MUX_S2, i&0b0100); //3nd bit of i controls S2
+      //digitalWrite(MUX_S3, i&0b1000); //3nd bit of i controls S2
+      
+      delay(200);
       
       ADCs[i] = analogRead(ADC_pin[0]);
       Voltages[i] = ADCs[i]* 3.3 /1023;
@@ -87,14 +90,16 @@ void loop() {
   Serial.print("V ");
   Serial.print("V1=");
   Serial.print(Voltages[1]);
-  Serial.print("V ");
+  Serial.print("V \n");
+  /*
   Serial.print("V2=");
   Serial.print(Voltages[2]);
   Serial.print("V ");
   Serial.print("V3=");
   Serial.print(Voltages[3]);
   Serial.print("V\n");
-
+  */
+/*
   Serial.print("V4=");
   Serial.print(Voltages[4]);
   Serial.print("V ");
@@ -107,19 +112,23 @@ void loop() {
   Serial.print("V7=");
   Serial.print(Voltages[7]);
   Serial.print("V\n");
-  
+*/
+
   Serial.print("T0=");
   Serial.print(Temperatures[0]);
   Serial.print("C ");
   Serial.print("T1=");
   Serial.print(Temperatures[1]);
-  Serial.print("C ");
+  Serial.print("C \n");
+/*
   Serial.print("T2=");
   Serial.print(Temperatures[2]);
   Serial.print("C ");
   Serial.print("T3=");
   Serial.print(Temperatures[3]);
   Serial.print("C\n");
+*/
+  
   /*
   Serial.print("V_Max at pin");
   Serial.print(maxValueADCindex);
@@ -129,8 +138,8 @@ void loop() {
 */
 
   //delay program before
-  delay(1500);
+  //delay(1500);
   digitalWrite(ledPin, 0); 
-  delay(100);
+  delay(300);
   digitalWrite(ledPin, 1); 
 }
